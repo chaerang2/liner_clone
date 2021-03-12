0f1b5152a73301cca99021aea7ef93ab81b1e84a
unknown <codbs54@gmail.com>
2021-03-12 20:19:44 +0900

    feature/#4/LeftMenu 왼쪽 사이드 메뉴 Link 및 업그레이드 버튼 구현

---
+++ src/components/LeftMenu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Link
+} from "react-router-dom";
+
+function LeftMenu(){
+  return(
+    <>
+      <ul>
+        <li><Link to='/'>For You</Link></li>
+        <li><Link to='/myhighlights'>My Highlights</Link></li>
+        <li><Link to='/more'>More</Link></li>
+      </ul>
+      <button>Upgrade</button>
+    </>
+  )
+}
+
+export default LeftMenu;
