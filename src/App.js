754fdcb9c37a5d2ec637eb7a22a88fdad57e5566
unknown <codbs54@gmail.com>
2021-03-12 21:36:12 +0900

    feature/#6/RightMenu fakedata생성 및 오른쪽 사이드 메뉴 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 import {
   BrowserRouter as Router,
 } from "react-router-dom";
-import Header from './components/Header'
+import Header from "./components/Header"
+import RightMenu from "./components/RightMenu"
 
 
 function App() {
   return (
     <Router>
       <div>
-        <h1>Liner</h1>
-        <Header></Header>
+        <Header />
+        <RightMenu />
       </div>
     </Router>
   );
