ce54299ae48ac75e7b50b0d929b0dca60ff93fea
unknown <codbs54@gmail.com>
2021-03-12 19:48:17 +0900

    feature/#1/Header react-router-dom설치 로고링크태그, 검색창을 위한 인풋태그, 그 외 버튼

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  Link
+} from "react-router-dom";
+
+function Header(){
+  return(
+    <>
+      <Link to="/">로고</Link>
+      <input placeholder="Search on LINER"></input>
+      <span>검색</span>
+      <button>종</button>
+      <button>+PDF</button>
+    </>
+  )
+}
+
+export default Header;
